0a8a375ef1f70250079c84788f435abf22b14050
Vishal Tailor <vdtailor@gmail.com>
2024-01-09 16:59:40 -0500

    Create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongogoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User");
+
+export default User;
